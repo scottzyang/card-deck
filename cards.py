3594ac53d296005a804982ed03cc30420185fa0f
scottzyang <scottzyang@gmail.com>
2022-11-02 18:00:38 -0500

    Create Cards Class

--- cards.py
+++ cards.py
@@ -1,2 +1,10 @@
+import random
 
+class Card:
+    def __init__(self, suit, value, color):
+        self.suit = suit
+        self.value = value
+        self.color = color
 
+    def show(self):
+        print(f"Suit: {self.suit}\nValue: {self.value}\nColor: {self.color}")
