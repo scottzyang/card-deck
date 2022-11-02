4f21784b7b1d957e5b47eccdcc45c648441d06d1
scottzyang <scottzyang@gmail.com>
2022-11-02 18:01:28 -0500

    Create Deck class and create_deck method

--- cards.py
+++ cards.py
@@ -8,3 +8,25 @@ class Card:
 
     def show(self):
         print(f"Suit: {self.suit}\nValue: {self.value}\nColor: {self.color}")
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+    def create_deck(self):
+        suits = ['hearts', 'diamonds', 'spades', 'clubs']
+
+        for suit in suits:
+            for i in range(1, 14):
+                # print(i)
+                if suit == 'hearts' or suit == 'diamonds':
+                    temp = Card(suit, i, 'red')
+                else: 
+                    temp = Card(suit, i, 'black')
+                self.cards.append(temp)
+        
+        # test to see if cards list was populated
+        # for card in self.cards:
+        #     print(card.value, card.suit, card.color)
+                
